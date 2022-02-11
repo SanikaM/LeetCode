c244e9f64f411ba161b54db5767676766c9617e6
SanikaM <sanika.mathapati215@gmail.com>
2022-02-11 16:28:47 -0500

    Time: 11 ms (68.09%), Space: 49.2 MB (20.81%) - LeetHub

---
+++ 696-count-binary-substrings/696-count-binary-substrings.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countBinarySubstrings(String s) {
+        
+        int prev =0, curr = 1, ans =0;
+        for(int i=1;i<s.length();i++){
+            if(s.charAt(i-1)==s.charAt(i)) curr++;
+            else{
+                ans += Math.min(prev,curr);
+                prev=curr;
+                curr =1;
+            }
+        }
+        
+         return ans + Math.min(curr, prev);
+    }
+}
