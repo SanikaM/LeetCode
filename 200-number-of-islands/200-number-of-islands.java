abca855add35de10a9196b5bb6e67d1e336de87c
SanikaM <sanika.mathapati215@gmail.com>
2022-02-15 18:36:17 -0500

    Time: 5 ms (54.71%), Space: 57 MB (21.60%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -3,30 +3,28 @@ class Solution {
         
         if(grid == null || grid.length == 0)
             return 0;
+    
+        int num =0;
         
-        int num=0;
-        
-        for(int i=0;i<grid.length;i++)
-        {
-            for(int j=0;j<grid[i].length;j++){
-                if(grid[i][j]=='1')
-                  num = num + dfs(grid, i, j);   
+        for(int i=0; i< grid.length;i++) {
+            for(int j=0;j<grid[i].length;j++) {
+                
+                num = num + dfs(grid, i, j);
             }
+          
         }
-        return num;
-        
+          return num;
     }
-    public int dfs(char grid[][], int i, int j)
-        {
-         
-            if(i<0 || i>= grid.length || j<0 || j>= grid[i].length || grid[i][j]=='0')
-                return 0;
-            
-            grid[i][j]='0';
-            dfs(grid, i+1, j);
-            dfs(grid, i-1, j);
-            dfs(grid, i, j+1);
-            dfs(grid, i, j-1);
-            return 1;
-        }
+    
+    public int dfs(char[][] grid, int i, int j) {
+        
+        if(i<0 || i>=grid.length || j<0 || j>=grid[i].length || grid[i][j] == '0' )
+            return 0;
+        grid[i][j] = '0';
+        dfs(grid, i+1, j);
+        dfs(grid, i-1, j);
+        dfs(grid, i, j-1);
+        dfs(grid, i, j+1);
+        return 1;
+    }
 }
