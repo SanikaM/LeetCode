e4b8368d0a39e574a3d025d53500a49257979896
SanikaM <sanika.mathapati215@gmail.com>
2022-02-16 13:24:01 -0500

    Time: 6 ms (64.82%), Space: 48.9 MB (20.01%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.java
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -3,15 +3,14 @@ class Solution {
     public boolean dfs(int u,List<Integer>[] adj,List<Integer> s,int[] visited){
         
         visited[u] = 1;
-        for(int i: adj[u])
-        {
+        for(int i: adj[u]) {
             if(visited[i] == 1)
                 return true;
-            if(visited[i] == 0 && dfs(i,adj,s,visited))
+            if(visited[i] ==0 && dfs (i,adj,s,visited))
                 return true;
             
         }
-        visited[u] = 2;
+        visited[u] =2;
         s.add(u);
         return false;
         
@@ -23,23 +22,25 @@ class Solution {
         for(int i=0;i<numCourses;i++)
             adj[i] = new LinkedList<Integer>();
         
-        for(int[] courses : prerequisites) 
-            adj[courses[1]].add(courses[0]);
-        
-        List<Integer> s = new LinkedList();
+        List<Integer> s = new ArrayList<>();
         int[] visited = new int[numCourses];
+        
+        for(int[] pre: prerequisites)
+            adj[pre[1]].add(pre[0]);
         
         for(int i=0;i<numCourses;i++) {
             if(visited[i] == 0 && dfs(i,adj,s,visited))
                 return new int[0];
         }
+        
         Collections.reverse(s);
         int[] result = new int[s.size()];
         int c =0;
-        for(int val : s){
+        for(int val : s) {
             result[c] = val;
             c++;
-        }        
+        }
+        
         return result;
     }
 }
