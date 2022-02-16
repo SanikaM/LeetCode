053797775945f43a78d27d77a04d856056479b69
SanikaM <sanika.mathapati215@gmail.com>
2022-02-16 15:34:36 -0500

    Time: 9 ms (50.53%), Space: 43.6 MB (65.17%) - LeetHub

--- 735-asteroid-collision/735-asteroid-collision.java
+++ 735-asteroid-collision/735-asteroid-collision.java
@@ -5,24 +5,24 @@ class Solution {
         int i=0;
         while(i<asteroids.length) {
             
-            if(asteroids[i]>0)
+            if(asteroids[i] >0)
                 stack.push(asteroids[i]);
-            else{
-                while(!stack.isEmpty() && stack.peek() > 0 && Math.abs(asteroids[i]) > stack.peek())
-                    stack.pop();
+            else {
                 
-                if(stack.isEmpty() || stack.peek() < 0)
-                    stack.push(asteroids[i]);
-                else if(Math.abs(asteroids[i]) == stack.peek())
+                while(!stack.isEmpty() && stack.peek()>0 && stack.peek() < Math.abs(asteroids[i]))
                     stack.pop();
+                if( stack.isEmpty() || stack.peek()<0)
+                    stack.push(asteroids[i]);
+                else if(stack.peek() == Math.abs(asteroids[i]))
+                    stack.pop();
+                 
             }
             i++;
         }
         
         int[] remaining = new int[stack.size()];
-        for(int j = stack.size()-1 ; j>=0; j--) {
+        for(int j=stack.size()-1;j>=0;j--)
             remaining[j] = stack.pop();
-        }
         
         return remaining;
     }
