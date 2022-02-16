25ab759d514cdccb4c8292594aba3af86f890b65
SanikaM <sanika.mathapati215@gmail.com>
2022-02-16 17:28:44 -0500

    Time: 10 ms (57.66%), Space: 42.1 MB (71.33%) - LeetHub

--- 909-snakes-and-ladders/909-snakes-and-ladders.java
+++ 909-snakes-and-ladders/909-snakes-and-ladders.java
@@ -1,53 +1,52 @@
 class Solution {
     public int snakesAndLadders(int[][] board) {
-        
         int n = board.length;
+        Map<Integer,Integer> map = new HashMap<>();
+        
         int start = n*n;
         
-        Map<Integer,Integer> hm = new HashMap<>();
-        
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++){
-                if ((n-i) % 2 == 0) {
-                   hm.put(start, board[i][j]);
-                } else {
-                   hm.put(start, board[i][n - j - 1]);
-                }
-                start--;
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++) {
+                if((n-i)%2 ==0)
+                    map.put(start, board[i][j]);
+                else
+                    map.put(start, board[i][n-j-1]);
+            start--;
             }
         }
         
-        Queue<Integer> queue = new LinkedList<Integer>();
+        Queue<Integer> queue = new LinkedList<>();
         HashSet<Integer> visited = new HashSet<>();
         queue.add(1);
         visited.add(1);
         int step =0;
-        while(!queue.isEmpty()) {
+        
+        while(!queue.isEmpty()){
+            
             int size = queue.size();
             
-            for(int i=0;i<size;i++){
-            int current = queue.poll();
-            if(current == n*n)
-                return step;
-            
-            for(int j=1;j<=6;j++) {
+            for(int i=0;i<size;i++) {
                 
-                int newPoint = current + j;
+                int current = queue.poll();
+                if(current == n*n)
+                    return step;
                 
-                if(newPoint > n*n)
-                    break;
-                
-                if(hm.get(newPoint) != -1)
-                    newPoint = hm.get(newPoint);
-                
-                if(!visited.contains(newPoint)) {
-                    queue.add(newPoint);
-                    visited.add(newPoint);
+                for(int j=1;j<=6;j++) {
+                    
+                    int newPoint = current+ j;
+                    
+                    if(newPoint > n*n)
+                        break;
+                    
+                    if(map.get(newPoint) != -1)
+                        newPoint = map.get(newPoint);
+                    if(!visited.contains(newPoint)) {
+                        queue.add(newPoint);
+                        visited.add(newPoint);
+                    }
                 }
             }
-            }
-                step++;
-            
+            step++;
         }
         return -1;
     }
