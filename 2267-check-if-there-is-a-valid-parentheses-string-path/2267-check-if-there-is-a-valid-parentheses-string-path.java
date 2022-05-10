9ea96ac6252262799e2cf82f5f8483dbc829afdf
SanikaM <sanika.mathapati215@gmail.com>
2022-05-10 01:16:39 -0400

    Time: 1518 ms (5.10%), Space: 107.6 MB (53.28%) - LeetHub

--- 2267-check-if-there-is-a-valid-parentheses-string-path/2267-check-if-there-is-a-valid-parentheses-string-path.java
+++ 2267-check-if-there-is-a-valid-parentheses-string-path/2267-check-if-there-is-a-valid-parentheses-string-path.java
@@ -10,35 +10,26 @@ class Solution {
     
     private boolean dfs(int i, int j, char[][] grid, int openCount) {
         
-        if(i<0 || i >= grid.length || j<0 || j>=grid[0].length) 
+          if(i<0 || i >= grid.length || j<0 || j>=grid[0].length) 
             return false;
-        
-        char c = grid[i][j];
-        if(c == '(')
-            openCount++;
-        else
-            openCount--;
-        
-        if(i == grid.length-1 && j == grid[0].length-1)
-        {
-            if(openCount == 0)
-                return true;
+        if(i == grid.length-1 && j == grid[0].length-1) {
             
+            if(openCount == 1 && grid[i][j] == ')')
+                return true;
+            else
+                return false;
         }
-        
-        if(openCount <0 || openCount > ((grid.length + grid[0].length -1)/2 ))
+        if(openCount < 0)
             return false;
-        
         String str = Integer.toString(i) +'*'+Integer.toString(j)+'*'+Integer.toString(openCount);
-        
         if(map.containsKey(str)) {
            // System.out.println("I am in");
             return map.get(str);
         }
-        
-        
-            map.put(str, dfs(i+1,j,grid,openCount) || dfs(i,j+1,grid,openCount));
-       
+        if(grid[i][j] == '(') 
+            map.put(str, dfs(i+1,j,grid,openCount+1) || dfs(i,j+1,grid,openCount+1));
+        else
+            map.put(str, dfs(i+1,j,grid,openCount-1) || dfs(i,j+1,grid,openCount-1));
         
         return map.get(str);
     }
