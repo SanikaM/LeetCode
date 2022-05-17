0a7caa69623e7bd82a14341d92d1ef1aeb12cb48
SanikaM <sanika.mathapati215@gmail.com>
2022-05-17 15:50:59 -0400

    Time: 9 ms (56.76%), Space: 42.5 MB (74.53%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -1,36 +1,25 @@
 class Solution {
     
-    /*
-    I had declared the map inside the method first. which again gave TLE.
-Remember this !!! -- Declare map outside, you dont wanna create the map everytime the recursive call takes place.
-    */
+     Map<String, Boolean> map = new HashMap<>();
     
-    public Map<String, Boolean> map = new HashMap<>();
     public boolean wordBreak(String s, List<String> wordDict) {
-     
+        
+       
         if(wordDict.contains(s))
             return true;
-        
-        
-        
         if(map.containsKey(s))
             return map.get(s);
         
-        for(int i =1; i<= s.length(); i++) {
+        for(int i =0; i<s.length(); i++) {
             
             String left = s.substring(0,i);
-            if(wordDict.contains(left) && wordBreak(s.substring(i), wordDict))
-            {
+            if(wordDict.contains(left) && wordBreak(s.substring(i), wordDict)) {
                 map.put(s, true);
                 return true;
             }
-            
-                                          
         }
-         map.put(s,false);   
+        
+        map.put(s, false);
         return false;
-       
     }
-    
-    
 }
