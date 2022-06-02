b8ca6d7479abf024dd2b8b51a2de789a3dea2da1
SanikaM <sanika.mathapati215@gmail.com>
2022-06-02 14:29:14 -0400

    Time: 7 ms (40.82%), Space: 54.8 MB (17.98%) - LeetHub

--- 490-the-maze/490-the-maze.java
+++ 490-the-maze/490-the-maze.java
@@ -10,22 +10,31 @@ class Solution {
         
         while(!queue.isEmpty()) {
             
-            int[] s = queue.remove();
-            if (s[0] == destination[0] && s[1] == destination[1])
+            int[] curr = queue.remove();
+            
+            if(curr[0] == destination[0] && curr[1] == destination[1])
                 return true;
-            for (int[] dir: dirs) {
-                int x = s[0] + dir[0];
-                int y = s[1] + dir[1];
-                while (x >= 0 && y >= 0 && x < maze.length && y < maze[0].length && maze[x][y] == 0) {
+            
+            for(int[] dir : dirs) {
+                
+                int x = curr[0] + dir[0];
+                int y = curr[1] + dir[1];
+                
+                while(x >=0 && y >=0 && x<maze.length && y<maze[0].length && maze[x][y] == 0) { 
                     x += dir[0];
                     y += dir[1];
                 }
-                if (!visited[x - dir[0]][y - dir[1]]) {
-                    queue.add(new int[] {x - dir[0], y - dir[1]});
-                    visited[x - dir[0]][y - dir[1]] = true;
+                
+                if(!visited[x-dir[0]][y-dir[1]]) {
+                    
+                    queue.add(new int[] {x-dir[0], y-dir[1]});
+                    visited[x-dir[0]][y-dir[1]] = true;
                 }
+                
             }
+            
         }
+        
         return false;
     }
 }
