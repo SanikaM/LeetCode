92bcfb68af78e53cc67bd862699da3861927401d
SanikaM <sanika.mathapati215@gmail.com>
2022-07-28 13:18:27 -0400

    Time: 44 ms (73.41%), Space: 42.1 MB (84.99%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.java
+++ 279-perfect-squares/279-perfect-squares.java
@@ -1,32 +1,36 @@
 class Solution {
-  public int numSquares(int n) {
-
-    ArrayList<Integer> square_nums = new ArrayList<Integer>();
-    for (int i = 1; i * i <= n; ++i) {
-      square_nums.add(i * i);
-    }
-
-    Set<Integer> queue = new HashSet<Integer>();
-    queue.add(n);
-
-    int level = 0;
-    while (queue.size() > 0) {
-      level += 1;
-      Set<Integer> next_queue = new HashSet<Integer>();
-
-      for (Integer remainder : queue) {
-        for (Integer square : square_nums) {
-          if (remainder.equals(square)) {
-            return level;
-          } else if (remainder < square) {
-            break;
-          } else {
-            next_queue.add(remainder - square);
-          }
+    public int numSquares(int n) {
+        
+        int levels = 0;
+        
+        ArrayList<Integer> sqrs = new ArrayList<>();
+        
+        for(int i=1; i * i <= n ; ++i) 
+            sqrs.add(i*i);
+        
+        Set<Integer> queue = new HashSet<>();
+        queue.add(n);
+        
+        while(queue.size() > 0) {
+            
+            levels += 1;
+            Set<Integer> q = new HashSet<>();
+            
+            for(Integer rem : queue) {
+                for(Integer sqr : sqrs) {
+                    
+                    if(rem.equals(sqr))
+                        return levels;
+                    else if(rem < sqr)
+                        break;
+                    else
+                        q.add(rem - sqr);
+                }
+            }
+            
+            queue = q;
         }
-      }
-      queue = next_queue;
+        
+        return levels;
     }
-    return level;
-  }
 }
